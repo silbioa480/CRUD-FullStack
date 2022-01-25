8ae13cd11592bd9098626d2b244bdf206dfc8c4c
silbioa480 <silbioa480@gmail.com>
2022-01-25 23:19:03 +0900

    add ListEmployeeComponent

---
+++ crud_frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,30 @@
+function ListEmployeeComponent({ employees }) {
+  return (
+    <div>
+      <h2 className="text-center">Employees List</h2>
+      <div className="row">
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Employee First Name</th>
+              <th>Employee Last Name</th>
+              <th>Employee Email Id</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.employees.map((employee) => {
+              <tr key={employee.id}>
+                <td>{employee.firstName}</td>
+                <td>{employee.lastName}</td>
+                <td>{employee.emailId}</td>
+              </tr>;
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default ListEmployeeComponent;
