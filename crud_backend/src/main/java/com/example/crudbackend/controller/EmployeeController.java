8df61a5b3b2a1ac7c06241ae9e2b53f2dee4ceb2
silbioa480 <silbioa480@gmail.com>
2022-01-26 16:52:48 +0900

    add getEmployeeById

--- crud_backend/src/main/java/com/example/crudbackend/controller/EmployeeController.java
+++ crud_backend/src/main/java/com/example/crudbackend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.crudbackend.controller;
 
+import com.example.crudbackend.exception.ResourceNotFoundException;
 import com.example.crudbackend.model.Employee;
 import com.example.crudbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +27,13 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+
+    // get employee by id rest api
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+        return ResponseEntity.ok(employee);
     }
 }
